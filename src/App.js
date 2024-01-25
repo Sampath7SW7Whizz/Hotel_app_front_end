ed8f736fc7497128caa4dc68c6aa26d574f6d253
Sampathar7 <sampathblnkgm@gmail.com>
2024-01-25 23:05:33 +0530

    second

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import AddRoom from './components/room/AddRoom';
 import RoomTypeSelector from './components/common/RoomTypeSelector';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import EditRoom from './components/room/EditRoom';
+import ExistingRooms from './components/room/ExistingRooms';
 
 function App() {
   return(
     <>
-    <AddRoom/> 
+    <main>
+      <Router>
+        <Routes>
+        <Route path="/" element={<Home />} />
+				<Route path="/edit-room/:roomId" element={<EditRoom />} />
+				<Route path="/existing-rooms" element={<ExistingRooms />} />
+        </Routes>
+      </Router>
+     </main>
     </>
   )
 }
