9891597236da55e6f6f110c33fece2a5153e1f1f
Sampathar7 <sampathblnkgm@gmail.com>
2024-01-25 18:27:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
+import AddRoom from './components/room/AddRoom';
+import RoomTypeSelector from './components/common/RoomTypeSelector';
+import { Router } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <>
+    <AddRoom/> 
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/utils/ApiFunctions.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+
+export const api = axios.create({
+	baseURL: "http://localhost:9192"
+})
+
+export const getHeader = () => {
+	const token = localStorage.getItem("token")
+	return {
+		Authorization: `Bearer ${token}`,
+		"Content-Type": "application/json"
+	}
+}
+
+/* This function adds a new room room to the database */
+export async function addRoom(photo, roomType, roomPrice) {
+	const formData = new FormData()
+	formData.append("photo", photo)
+	formData.append("roomType", roomType)
+	formData.append("roomPrice", roomPrice)
+
+	const response = await api.post("/rooms/add/new-room", formData,{
+		headers:getHeader()
+	})
+	if (response.status === 201) {
+		return true
+	} else {
+		return false
+	}
+}
+
+/* This function gets all room types from thee database */
+export async function getRoomTypes() {
+	try {
+		const response = await api.get("/rooms/room/types")
+		return response.data
+	} catch (error) {
+		throw new Error("Error fetching room types")
+	}
+}
+
+/* This function gets all rooms from the database */
+export async function getAllRooms() {
+	try {
+		const result = await api.get("/rooms/all-rooms")
+		return result.data
+	} catch (error) {
+		throw new Error("Error fetching rooms")
+	}
+}
+
+/* This function deletes a room by the Id */
+export async function deleteRoom(roomId) {
+	try {
+		const result = await api.delete(`/rooms/delete/room/${roomId}`, {
+			headers: getHeader()
+		})
+		return result.data
+	} catch (error) {
+		throw new Error(`Error deleting room ${error.message}`)
+	}
+}
+
+
+
+
+
